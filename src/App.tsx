f9ba036bdae0df3b90126e360f490197d18cf2b9
Gleuton martins <caiohmendessilva@gmail.com>
2023-05-17 11:52:32 -0300

    estrutura do e-commerce

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import Home from './pages/home/Home';
+import './App.css';
+import Login from './pages/login/Login';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
+      <Footer />
+    </Router>
+
+  );
+}
+
+export default App;
