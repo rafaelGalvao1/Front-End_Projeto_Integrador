5006ba3a2a408b6968de2c90e0fb2bc7c7033d00
Rafael Galvão <rafinha_windows@hotmail.com>
2023-05-31 12:22:21 -0300

    Sobre Nos

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -5,16 +5,36 @@ import './Sobre.css';
 function Sobre() {
     return (
         <>
-        <Grid container direction={'row'} justifyContent={'center'} alignItems={'center'}>
-                <Grid item xs={6} alignItems={'center'}>
-                <Box paddingX={20}>
+            <Box>
+                <Typography variant='h4' gutterBottom color='textPrimary' component='h4' align='center' style={{ fontWeight: 'bold' }}>
+                    Sobre o Terra Verde.com
+                </Typography>
+            </Box>
+            <Grid container direction={'row'} justifyContent={'center'} alignItems={'center'}>
 
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight:'bold'}}>Sobre nós</Typography>
-                        <Typography variant='h6' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight:'bold'}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis et nemo laudantium numquam esse omnis unde voluptas. Exercitationem ullam quibusdam modi tenetur iure in quidem et, nihil sunt, totam dolor. </Typography>
-
-                </Box>
+                <Grid item xs={5}>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <img src="https://i.imgur.com/DMJSJjx.jpg" alt="Logo" className="img1" />
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <img src="https://i.imgur.com/cOqH7XU.jpg" alt="Fazenda" className='img2'/>
+                    </Box>
                 </Grid>
-                <Grid className='imageLogo' item xs={6}>  </Grid>
+                <Grid item xs={7} alignItems={'center'}>
+                    <Box paddingX={10} className="margin">
+
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>A Missão do Terra Verde</Typography>
+                        <Typography variant='h6' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis et nemo laudantium numquam esse omnis unde voluptas. Exercitationem ullam quibusdam modi tenetur iure in quidem et, nihil sunt, totam dolor. </Typography>
+
+                    </Box>
+                    <Box paddingX={10} className="margin">
+
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}>Quem Somos Nos?</Typography>
+                        <Typography variant='h6' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis et nemo laudantium numquam esse omnis unde voluptas. Exercitationem ullam quibusdam modi tenetur iure in quidem et, nihil sunt, totam dolor. </Typography>
+
+                    </Box>
+                </Grid>
+
             </Grid>
         </>
     )
